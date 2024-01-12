613967e42ec6923c3b861db8c2f3f0f13bf70705
ivanalfonso97 <ivanalfonso.chan@gmail.com>
2024-01-12 22:42:57 +0800

    create authentication and permissions

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,20 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/prescriptions/"
+auth_endpoint = "http://localhost:8000/api/auth/"
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+username = input("Username: ")
+password = getpass("Password: ")
+
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password})
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/prescriptions/"
+
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
